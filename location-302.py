8ff6267bef320dc0a6e86458aa428acc1de4eccc
dmitrik <dmitrik@ironsrc.com>
2015-05-21 09:27:05 +0300

    substring token

--- location-302.py
+++ location-302.py
@@ -3,9 +3,9 @@ import urllib
 
 def location302Url(id, secret, url):
 	serviceUrl = "http://302-location.com"
-	redirectUrl = urllib.quote_plus(url)
 
-	params = "id=" + str(id) + "&url=" + redirectUrl
+	params = "i=" + str(id) + "&u=" + urllib.quote_plus(url)
 	token = hashlib.sha256(secret + params).hexdigest()
+	token = token[0:4]
 
-	return serviceUrl + "/?" + params + "&token=" + token
+	return serviceUrl + "/?" + params + "&t=" + token
