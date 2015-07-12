5af489e7095748723cb3fced7de26077e118f1aa
dmitrik <dmitrik@ironsrc.com>
2015-07-12 18:43:27 +0300

    remove id from token

--- location-302.py
+++ location-302.py
@@ -5,7 +5,7 @@ def location302Url(id, secret, url):
 	serviceUrl = "http://302-location.com"
 
 	id = str(id)
-	token = hashlib.sha256(secret+id+url).hexdigest()
+	token = hashlib.sha256(secret+url).hexdigest()
 	token = token[0:4]
 
 	params = "i=" + id + "&u=" + urllib.quote_plus(url) + "&t=" + token
